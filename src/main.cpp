0871ad0122c35940d7b047340bd0ad839020496c
gugugu625 <18210965650@163.com>
2023-04-17 23:51:41 +0800

    Add Arduino GFX test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Adafruit_MCP23X17.h>
+#include <Arduino_GFX_Library.h>
 
 Adafruit_MCP23X17 mcp;
 #define INT_PIN 37
@@ -31,7 +32,15 @@ void setup() {
   Serial.begin(115200);
   MCPButtonInit();
 
-
+  //Arduino_DataBus *bus = new Arduino_ESP32PAR16(
+  //    21 /* DC */,33 /* CS */, 16 /* WR */, 17 /* RD */,
+  //    0 /* D0 */, 1 /* D1 */, 2 /* D2 */, 3 /* D3 */, 4 /* D4 */, 5 /* D5 */, 6 /* D6 */, 7 /* D7 */,
+  //    8 /* D8 */, 9 /* D9 */, 10 /* D10 */, 11 /* D11 */, 12 /* D12 */, 13 /* D13 */, 14 /* D14 */, 15 /* D15 */);
+  Arduino_DataBus *bus = new Arduino_ESP32S2PAR16(21/* DC */, 33 /* CS */, 16 /* WR */, 17 /* RD */);
+  Arduino_GFX *gfx = new Arduino_ILI9488(
+    bus, 18 /* RST */, 0 /* rotation */, false /* IPS */);
+  gfx->begin();
+  gfx->println(F("Arduino GFX Test"));
 }
 
 void loop() {
